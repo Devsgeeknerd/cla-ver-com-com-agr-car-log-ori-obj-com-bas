6a50fc0f9a2c25fae0b3fdf253670f56d5cbdb51
Paulo Vanderley <vanderley.1109@gmail.com>
2023-06-21 13:40:18 -0300

    adicionando conteudo

---
+++ Programa.java
@@ -0,0 +1,41 @@
+package br.com.devsgeeknerd.zoologico.app;
+
+import br.com.devsgeeknerd.zoologico.classes.Animal;
+import br.com.devsgeeknerd.zoologico.classes.Cachorro;
+import br.com.devsgeeknerd.zoologico.classes.Gato;
+import br.com.devsgeeknerd.zoologico.classes.Zoologico;
+
+public class Programa {
+    public static void main(String[] args) {
+        Animal Animal = new Cachorro("Toto");
+        System.out.println("Ola, seu animal é " + Animal.getEspecie() + ", o nome do " + "animal é " + Animal.getNome()
+                + " e ele tem " + Animal.getIdade() + " anos.");
+
+        if (Animal.ehAdulto()) {
+            System.out.println("Seu cachorro é adulto");
+        } else {
+            System.out.println("Seu cachorro nao é adulto");
+        }
+
+        System.out.println("============");
+        System.out.println("Barulho do cachorro: ");
+        Animal.emitirBarulho();
+
+        System.out.println("*****************************");
+        Zoologico zoo = new Zoologico();
+        zoo.setNome("Zoo Devsgeeknerd");
+        zoo.adicionarAnimal(Animal);
+
+        Animal animal2 = new Gato("Pintado", 3);
+        zoo.adicionarAnimal(animal2);
+
+        System.out.println("Animais do zoologico: " + zoo.getNome());
+        zoo.listarAnimais();
+        zoo.removerAnimal(0);
+
+        System.out.println("Depois da remocao: ");
+        zoo.listarAnimais();
+
+    }
+
+}
